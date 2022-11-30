3c914f690938ac1fafb2f729bf492137cd553910
Kayré Scott Primon <64801417+Kayre-Scott-Primon@users.noreply.github.com>
2022-11-30 18:22:22 -0300

    Styles and implementation API

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../api';
 import { Styles } from './style';
 
@@ -8,9 +8,19 @@ const Home = () => {
         readApi()
     }, [])
 
+    const [response, setResponse] = useState('')
+    const [responseAdj, setResponseAdj] = useState('')
+
     function readApi() {
         api.get('insult').then((res) => {
             console.log('resposta: ', res.data)
+            setResponse(res.data)
+        }).catch((e) => {
+            console.log('err 01', e)
+        })
+        api.get('adjective').then((res) => {
+            console.log('resposta: ', res.data)
+            setResponseAdj(res.data)
         }).catch((e) => {
             console.log('err 01', e)
         })
@@ -23,6 +33,25 @@ const Home = () => {
                     Free Insults
                 </Styles.Title>
             </Styles.ViewTitle>
+
+            <Styles.ViewResponse>
+                <Styles.Response>
+                    {response}
+                </Styles.Response>
+            </Styles.ViewResponse>
+
+
+            <Styles.ViewResponse>
+                <Styles.Response>
+                    Adjective: {responseAdj}
+                </Styles.Response>
+            </Styles.ViewResponse>
+
+            <Styles.Button onPress={readApi}>
+                <Styles.ButtonLabel>
+                    Refresh
+                </Styles.ButtonLabel>
+            </Styles.Button>
 
         </Styles.Container>
     );
--- src/screens/home/style.js
+++ src/screens/home/style.js
@@ -14,12 +14,52 @@ export const Styles = {
         border-color: #fff;
         border-radius: 10px;
         padding: 5px 55px 5px 55px;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
     `,
 
     Title: styled.Text`
         font-size: 30px;
         color: #fff;
         font-weight: 600;
-    `
+    `,
 
+    ViewResponse: styled.View`
+        border-width: 2px;
+        border-color: #fff;
+        border-radius: 10px;
+        padding: 5px 10px 5px 10px;
+        margin-top: 25px;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    `,
+
+    Response: styled.Text`
+        font-size: 30px;
+        color: #fff;
+        font-weight: 600;
+        margin: 10px;
+    `,
+
+    Button: styled.TouchableOpacity`
+        border-width: 2px;
+        border-color: #fff;
+        border-radius: 10px;
+        padding: 10px 10px 10px 10px;
+        margin-top: 25px;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    `,
+
+    ButtonLabel: styled.Text`
+        font-size: 30px;
+        color: #fff;
+        font-weight: 800;
+    `
 }
